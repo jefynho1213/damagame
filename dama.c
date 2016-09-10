e3a8d4066bbc0fcd7973505c38aed1651331c87f
jefynho1213 <jefynho1213@hotmail.com>
2016-09-10 16:49:17 -0300

    Update dama.c

--- dama.c
+++ dama.c
@@ -37,7 +37,7 @@ char **incializa(){
 
 	for(i=0;i<8;i++)
 		table[i]= (char *)calloc(8,sizeof(char));
-	
+
 	for(i=0;i<8;i++)
 		for(j=0;j<8;j++)
 			table[i][j] = ' ';
@@ -46,27 +46,44 @@ char **incializa(){
 
 }
 void organiza(char **table){
-	
+
 	int i,j;
 
-	for(i=0;i<2;i++)
-		for(j=0;j<8;j++)
-			table[i][j] = 'X';
-		
-	for(i=6;i<8;i++)
-		for(j=0;j<8;j++)
-			table[i][j] = 'O';
-	
+	for(i=0;i<3;i++){
+        if(i%2 == 0){
+            j=1;
+         }else{
+            j=0;
+         }
+        while(j<8){
+            table[i][j] = 'X';
+            j+=2;
+        }
+
+    }
+
+	for(i=5;i<8;i++){
+        if(i%2 == 0){
+            j=1;
+         }else{
+            j=0;
+         }
+        while(j<8){
+            table[i][j] = 'O';
+            j+=2;
+        }
+    }
 }
 
 
 void printa_table(char **table){
 	int i,j;
+	printf(" 0  1  2  3  4  5  6  7 \n");
 	for(i=0;i<8;i++){
 		for(j=0;j<8;j++){
 			printf("[%c]", table[i][j]);
 		}
-		printf("\n");
+		printf(" %d\n", i);
 	}
 }
 
@@ -79,23 +96,23 @@ void printa_table(char **table){
 //retorna 1 para realizar a jogada
 //jogar 1 é o jogador da area inferior
 int verificacasa(char **table, int linha_p, int coluna_p, char jogador, int linha_a, int coluna_a){
-	
-	
+
+
 		if(table[linha_p][coluna_p] == 'O'){
 			return -1;
-			//verifica se o jogador esta realizando uma jogada na diagonal 
+			//verifica se o jogador esta realizando uma jogada na diagonal
 		} else if(linha_a != linha_p-1 || coluna_a != coluna_p-1){
 			return -1;
 			//verifica se o jogador esta realizando uma jogada na diagonal
 		} else if(coluna_a != coluna_p+1){
 			return -1;
-			//verifica se o jogador este relizando 
+			//verifica se o jogador este relizando
 		} else if(linha_a < linha_p || linha_a == linha_p ){
 			return -1;
 		} else {
 			return 1;
 		}
-	
+
 }
 
 
